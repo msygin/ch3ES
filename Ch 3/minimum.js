00c48f54846413a8240eac36fbe7daa702d890d1
Jason W Ellsworth <jasonellsworth48@gmail.com>
2019-06-21 18:29:01 +0800

    ch3es
    Just a couple of practice problems using functions.

---
+++ Ch 3/minimum.js
@@ -0,0 +1,23 @@
+//Recreating the Math.min() javascript function.
+ //function miniValue(y, x) {
+//     if (y > x) {
+//         return x;
+//     }
+//     else {
+//         return y;
+//     }
+// }
+
+//bean counter
+
+ function countBs(phrase){
+     var count = 0;
+     for (i = 0; i <= phrase.length; i++) {
+         if (phrase[i] === 'B') {
+         count++;
+         }
+     }
+     console.log(count);
+ } 
+
+
